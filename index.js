58ce4e5380732c249871e243623927c4b7067a19
rashidulub <rashidul016094@gmail.com>
2023-05-02 13:54:33 +0600

    catch chef id and category id

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.port || 5000;  
 
 const categories = require('./data/categories.json')
+const chef = require('./data/chef.json')
 
 app.use(cors())
 
@@ -14,6 +15,29 @@ app.get('/', (req, res)=>{
 
 app.get('/categories',(req,res)=>{
     res.send(categories)
+})
+
+app.get('/chef',(req,res)=>{
+    res.send(chef);
+})
+
+app.get('/chef/:id',(req,res)=>{
+    const id =req.params.id;
+    const selectedChef = chef.find(n=>n._id === id)
+    res.send(selectedChef);
+})
+
+app.get('/categories/:id',(req,res)=>{
+    const id =parseInt (req.params.id);
+    if(id ===0){
+        res.send(chef)
+    }
+    else{
+        const categoryChef = chef.filter(n=>parseInt(n.category_id) === id)
+        res.send(categoryChef)
+
+    }
+   
 })
 
 app.listen(port, ()=>{
